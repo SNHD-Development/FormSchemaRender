8a2175d7e3a56babded5111385437f767d1fb651
Prut Udomwattawee <apzentral@gmail.com>
2013-08-02 15:19:06 -0700

    Adding LightBox not minified.

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ require.config({
     "jquery.birthdaypicker": 'libs/jquery-form-plugin/bdate-picker',
     "jquery.wizard": 'libs/fuelux/wizard',
     "jquery.spinner": 'libs/fuelux/spinner',
-    "jquery.lightbox": 'libs/lightbox/lightbox-2.6.min',
+    "jquery.lightbox": 'libs/lightbox/lightbox-2.6',
 
     // Bootstrap Plugin
     "jquery.datepicker": 'libs/bootstrap-datepicker/bootstrap-datepicker',
