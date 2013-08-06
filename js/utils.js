8ecdf39f2e8750d0b3a0e611322cfce390239999
Prut Udomwattawee <apzentral@gmail.com>
2013-08-06 08:56:32 -0700

    Adding indexOf method if the browser is not implement this IE7.

--- js/utils.js
+++ js/utils.js
@@ -41,6 +41,15 @@ define([
 				}
 				return result;
 			};
+			// Array Method
+			if( ! Array.prototype.indexOf) {
+				Array.prototype.indexOf = function(obj, start) {
+					for (var i = (start || 0), j = this.length; i < j; i++) {
+						if (this[i] === obj) { return i; }
+					}
+					return -1;
+				};
+			}
 		},
 		// http://kevin.vanzonneveld.net
 		// +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
