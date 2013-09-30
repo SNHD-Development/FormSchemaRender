0fd73dd3e526f08bc188890f67d6aece58216e4c
Prut Udomwattawee <apzentral@gmail.com>
2013-09-30 14:00:13 -0700

    Adding languages features

--- js/main.js
+++ js/main.js
@@ -9,190 +9,195 @@
  **/
 
 require.config({
-  paths: {
-    // Major libraries
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
-    lodash: 'libs/lodash/lodash', // alternative to underscore
-    backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
-    bootstrap: 'libs/bootstrap/bootstrap',
-    json: 'libs/json/json3.min',
+    paths: {
+        // Major libraries
+        jquery: 'libs/jquery/jquery-min',
+        underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
+        lodash: 'libs/lodash/lodash', // alternative to underscore
+        backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
+        bootstrap: 'libs/bootstrap/bootstrap',
+        json: 'libs/json/json3.min',
 
-    // Backbone Plugin
-    modelbinder: 'libs/backbone-binder/Backbone.ModelBinder.min',
-    validation: 'libs/backbone-validation/backbone-validation-amd',
+        // Backbone Plugin
+        modelbinder: 'libs/backbone-binder/Backbone.ModelBinder.min',
+        validation: 'libs/backbone-validation/backbone-validation-amd',
 
-    // jQuery Plugin
-    "jquery.expose": 'libs/jquery-tools/toolbox.expose',
-    "jquery.overlay": 'libs/jquery-tools/jquery.tools.overlay.min',
-    "jquery.dateinput": 'libs/jquery-tools/jquery.tools.dateinput.min',
-    "jquery.ajaxsubmit": 'libs/jquery-form-plugin/jquery.form.min',
-    "jquery.placeholder": 'libs/jquery-form-plugin/jquery.placeholder',
-    "jquery.birthdaypicker": 'libs/jquery-form-plugin/bdate-picker',
-    "jquery.wizard": 'libs/fuelux/wizard',
-    "jquery.spinner": 'libs/fuelux/spinner',
-    "jquery.lightbox": 'libs/lightbox/lightbox-2.6',
-    "jquery.zclip": 'libs/copy/jquery.zclip.min',
+        // jQuery Plugin
+        "jquery.expose": 'libs/jquery-tools/toolbox.expose',
+        "jquery.overlay": 'libs/jquery-tools/jquery.tools.overlay.min',
+        "jquery.dateinput": 'libs/jquery-tools/jquery.tools.dateinput.min',
+        "jquery.ajaxsubmit": 'libs/jquery-form-plugin/jquery.form.min',
+        "jquery.placeholder": 'libs/jquery-form-plugin/jquery.placeholder',
+        "jquery.birthdaypicker": 'libs/jquery-form-plugin/bdate-picker',
+        "jquery.wizard": 'libs/fuelux/wizard',
+        "jquery.spinner": 'libs/fuelux/spinner',
+        "jquery.lightbox": 'libs/lightbox/lightbox-2.6',
+        "jquery.zclip": 'libs/copy/jquery.zclip.min',
 
-    // FileUpload
-    //'blueimp-helper': 'libs/file-upload/dependency/load-image.min',
-    //'load-image': 'libs/file-upload/dependency/load-image.min',
-    //'blueimp-gallery': 'libs/file-upload/dependency/blueimp-gallery',
-    //'jquery.blueimp-gallery': 'libs/file-upload/dependency/jquery.blueimp-gallery.min',
-    //'jquery.fileupload-process': 'libs/file-upload/jquery.fileupload-process',
-    //'jquery.ui.widget': 'libs/file-upload/jquery.ui.widget',
-    //'jquery.fileupload-image': 'libs/file-upload/jquery.fileupload-image',
-    //'jquery.fileupload-audio': 'libs/file-upload/jquery.fileupload-audio',
-    //'jquery.fileupload-video': 'libs/file-upload/jquery.fileupload-video',
-    //'jquery.fileupload-validate': 'libs/file-upload/jquery.fileupload-validate',
-    //'jquery.fileupload': 'libs/file-upload/jquery.fileupload',
-    //'jquery.fileupload-ui': 'libs/file-upload/jquery.fileupload-ui',
+        // FileUpload
+        //'blueimp-helper': 'libs/file-upload/dependency/load-image.min',
+        //'load-image': 'libs/file-upload/dependency/load-image.min',
+        //'blueimp-gallery': 'libs/file-upload/dependency/blueimp-gallery',
+        //'jquery.blueimp-gallery': 'libs/file-upload/dependency/jquery.blueimp-gallery.min',
+        //'jquery.fileupload-process': 'libs/file-upload/jquery.fileupload-process',
+        //'jquery.ui.widget': 'libs/file-upload/jquery.ui.widget',
+        //'jquery.fileupload-image': 'libs/file-upload/jquery.fileupload-image',
+        //'jquery.fileupload-audio': 'libs/file-upload/jquery.fileupload-audio',
+        //'jquery.fileupload-video': 'libs/file-upload/jquery.fileupload-video',
+        //'jquery.fileupload-validate': 'libs/file-upload/jquery.fileupload-validate',
+        //'jquery.fileupload': 'libs/file-upload/jquery.fileupload',
+        //'jquery.fileupload-ui': 'libs/file-upload/jquery.fileupload-ui',
 
-    // Bootstrap Plugin
-    "jquery.datepicker": 'libs/bootstrap-datepicker/bootstrap-datepicker',
+        // Bootstrap Plugin
+        "jquery.datepicker": 'libs/bootstrap-datepicker/bootstrap-datepicker',
 
-    // Require.js plugins
-    text: 'libs/require/text',
+        // Require.js plugins
+        text: 'libs/require/text',
 
-    // Just a short cut so we can put our html outside the js dir
-    // When you have HTML/CSS designers this aids in keeping them out of the js directory
-    templates: 'templates',
-    views: 'views'
-  },
-  shim: {
-    "bootstrap": {
-      deps: ['jquery'],
-      exports: 'jQuery'
+        // Just a short cut so we can put our html outside the js dir
+        // When you have HTML/CSS designers this aids in keeping them out of the js directory
+        templates: 'templates',
+        views: 'views'
     },
-    "jquery.expose": {
-      deps: ['jquery'],
-      exports: 'jQuery.expose'
-    },
-    "jquery.overlay": {
-      deps: ['jquery'],
-      exports: 'jQuery.overlay'
-    },
-    "jquery.dateinput": {
-      deps: ['jquery'],
-      exports: 'jQuery.dateinput'
-    },
-    "jquery.datepicker": {
-      deps: ['jquery', 'bootstrap'],
-      exports: 'jQuery.datepicker'
-    },
-    "jquery.wizard": {
-      deps: ['jquery', 'bootstrap'],
-      exports: 'jQuery.wizard'
-    },
-    "jquery.spinner": {
-      deps: ['jquery', 'bootstrap'],
-      exports: 'jQuery.spinner'
-    },
-    "jquery.ajaxsubmit": {
-      deps: ['jquery'],
-      exports: 'jQuery.ajaxSubmit'
-    },
-    "jquery.placeholder": {
-      deps: ['jquery'],
-      exports: 'jQuery.placeholder'
-    },
-    "jquery.birthdaypicker": {
-      deps: ['jquery'],
-      exports: 'jQuery.birthdaypicker'
-    },
-    "jquery.lightbox": {
-      deps: ['jquery'],
-      exports: 'jQuery.lightbox'
-    },
-    "jquery.zclip": {
-      deps: ['jquery'],
-      exports: 'jQuery.zclip'
+    shim: {
+        "bootstrap": {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        },
+        "jquery.expose": {
+            deps: ['jquery'],
+            exports: 'jQuery.expose'
+        },
+        "jquery.overlay": {
+            deps: ['jquery'],
+            exports: 'jQuery.overlay'
+        },
+        "jquery.dateinput": {
+            deps: ['jquery'],
+            exports: 'jQuery.dateinput'
+        },
+        "jquery.datepicker": {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'jQuery.datepicker'
+        },
+        "jquery.wizard": {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'jQuery.wizard'
+        },
+        "jquery.spinner": {
+            deps: ['jquery', 'bootstrap'],
+            exports: 'jQuery.spinner'
+        },
+        "jquery.ajaxsubmit": {
+            deps: ['jquery'],
+            exports: 'jQuery.ajaxSubmit'
+        },
+        "jquery.placeholder": {
+            deps: ['jquery'],
+            exports: 'jQuery.placeholder'
+        },
+        "jquery.birthdaypicker": {
+            deps: ['jquery'],
+            exports: 'jQuery.birthdaypicker'
+        },
+        "jquery.lightbox": {
+            deps: ['jquery'],
+            exports: 'jQuery.lightbox'
+        },
+        "jquery.zclip": {
+            deps: ['jquery'],
+            exports: 'jQuery.zclip'
+        }
     }
-  }
 });
 
 // Let's kick off the application
 
 require([
-  'jquery',
-  'views/app',
-  'vm',
-  'utils'
-], function($, AppView, Vm, Utils){
+    'jquery',
+    'views/app',
+    'vm',
+    'utils'
+], function($, AppView, Vm, Utils) {
 
-  $(function () {
-    Utils.setupOldBrowser();
+    $(function() {
+        Utils.setupOldBrowser();
 
-    var _mode, _view, _token, _opts, appView
-    , config = {
-      mode : ["read", "update", "create"],
-      view : ["default", "horizontal", "wizard"]
-    };
+        var _mode, _view, _token, _opts, appView, config = {
+                mode: ["read", "update", "create"],
+                view: ["default", "horizontal", "wizard"]
+            }, languages = languages || 'en';
 
-    if (typeof formSchema === 'undefined') {
-      throw 'formSchema is undefined';
-    }
-    // Cast to lowercase
-    Vm.toLower(formSchema);
-    if (typeof formData !== 'undefined') {
-      Vm.toLower(formData, ['fields', 'internalfields'] );
-      _mode = (typeof mode !== 'undefined' && config.mode.indexOf(mode.toLowerCase()) > -1) ? mode.toLowerCase(): 'update';
-      if (_mode === 'update') {
-        Vm.decodeHtml(formData);
-      }
-      if (typeof formData.internalfields !== 'undefined') {
-        formData.fields = _.extend(formData.fields, formData.internalfields);
-        delete formData.internalfields;
-      }
-    } else {
-      _mode = (typeof mode !== 'undefined' && config.view.indexOf(view.toLowerCase()) > -1) ? mode.toLowerCase(): 'create';
-    }    
+        if (typeof formSchema === 'undefined') {
+            throw 'formSchema is undefined';
+        }
+        // Cast to lowercase
+        Vm.toLower(formSchema);
+        // Change the Languages        
+        if (language && language !== 'en') {
+            Vm.changeLanguage(formSchema.fields, language);
+        }
+        if (typeof formData !== 'undefined') {
+            Vm.toLower(formData, ['fields', 'internalfields']);
+            _mode = (typeof mode !== 'undefined' && config.mode.indexOf(mode.toLowerCase()) > -1) ? mode.toLowerCase() : 'update';
+            if (_mode === 'update') {
+                Vm.decodeHtml(formData);
+            }
+            if (typeof formData.internalfields !== 'undefined') {
+                formData.fields = _.extend(formData.fields, formData.internalfields);
+                delete formData.internalfields;
+            }
+        } else {
+            _mode = (typeof mode !== 'undefined' && config.view.indexOf(view.toLowerCase()) > -1) ? mode.toLowerCase() : 'create';
+        }
 
-    _view = (typeof view !== 'undefined') ? view.toLowerCase(): 'horizontal';
-    _token = (typeof token !== 'undefined' && _mode !== 'read') ? token: '';
+        _view = (typeof view !== 'undefined') ? view.toLowerCase() : 'horizontal';
+        _token = (typeof token !== 'undefined' && _mode !== 'read') ? token : '';
 
-    _opts = {
-      formSchema : formSchema,
-      formData : ( (typeof formData === 'undefined') ? {}: formData ),
-      mode : _mode,
-      token : _token,
-      internal: ( (typeof internal === 'undefined') ? false: internal ),
-      hideButtons: ( (typeof hideButtons === 'undefined') ? false: hideButtons )
-    };
+        _opts = {
+            formSchema: formSchema,
+            formData: ((typeof formData === 'undefined') ? {} : formData),
+            mode: _mode,
+            token: _token,
+            internal: ((typeof internal === 'undefined') ? false : internal),
+            hideButtons: ((typeof hideButtons === 'undefined') ? false : hideButtons)
+        };
 
-    if (typeof formActionUrl !== 'undefined') {
-      _opts.formActionUrl = formActionUrl;
-    }
+        if (typeof formActionUrl !== 'undefined') {
+            _opts.formActionUrl = formActionUrl;
+        }
 
-    // Render Custom Script Here
-    if (typeof formEvents !== 'undefined') {
-      _.each(formEvents, function(value, key) {
-        $('div#app').on(formSchema.name+'.'+key, { Utils: Utils }, value);
-      });
-    }
+        // Render Custom Script Here
+        if (typeof formEvents !== 'undefined') {
+            _.each(formEvents, function(value, key) {
+                $('div#app').on(formSchema.name + '.' + key, {
+                    Utils: Utils
+                }, value);
+            });
+        }
 
-    // Clean Up Global Object
-    formSchema = null;
-    formData = null;
-    mode = null;
-    view = null;
-    token = null;
-    internal = null;
-    formEvents = null;
-    hideButtons = null;
-    formActionUrl = null;
+        // Clean Up Global Object
+        formSchema = null;
+        formData = null;
+        mode = null;
+        view = null;
+        token = null;
+        internal = null;
+        formEvents = null;
+        hideButtons = null;
+        formActionUrl = null;
 
-    // Setup View
-    _opts.formSchema.view = _view;
+        // Setup View
+        _opts.formSchema.view = _view;
 
-    // Check Browser
-    Utils.checkBrowser();
+        // Check Browser
+        Utils.checkBrowser();
 
-    appView = Vm.create({}, 'AppView', AppView, _opts);
+        appView = Vm.create({}, 'AppView', AppView, _opts);
 
-    $(appView.el).html('<p class="data-loader" style="text-align:center;margin: 20px;"><i class="icon-spinner icon-spin icon-large"></i> <span class="text-info">Loading Form Information ...</span></p>');
+        $(appView.el).html('<p class="data-loader" style="text-align:center;margin: 20px;"><i class="icon-spinner icon-spin icon-large"></i> <span class="text-info">Loading Form Information ...</span></p>');
 
-    appView.render();
-    
-  });
+        appView.render();
+
+    });
 });
--- js/vm.js
+++ js/vm.js
@@ -1,82 +1,90 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'events'
-], function($, _, Backbone, Events){
-  var views = {}
+    'jquery',
+    'underscore',
+    'backbone',
+    'events'
+], function($, _, Backbone, Events) {
+    var views = {}
 
-  , remove = function(name, clean) {
-    clean = clean || false;
-    if(typeof views[name] !== 'undefined') {
-      views[name].undelegateEvents();
-      if(typeof views[name].clean === 'function') {
-        views[name].clean();
-      }
-      if (clean) {
-        if(typeof views[name].removeContent === 'function') {
-          views[name].removeContent();
+        , remove = function(name, clean) {
+            clean = clean || false;
+            if (typeof views[name] !== 'undefined') {
+                views[name].undelegateEvents();
+                if (typeof views[name].clean === 'function') {
+                    views[name].clean();
+                }
+                if (clean) {
+                    if (typeof views[name].removeContent === 'function') {
+                        views[name].removeContent();
+                    }
+                }
+            }
         }
-      }
-    }
-  }
 
-  , create = function (context, name, View, options) {
-    // View clean up isn't actually implemented yet but will simply call .clean, .remove and .unbind
-    remove(name);
+        , create = function(context, name, View, options) {
+            // View clean up isn't actually implemented yet but will simply call .clean, .remove and .unbind
+            remove(name);
 
-    var view = new View(options);
-    views[name] = view;
-    if(typeof context.children === 'undefined'){
-      context.children = {};
-      context.children[name] = view;
-    } else {
-      context.children[name] = view;
-    }
-    Events.trigger('viewCreated');
-    return view;
-  }
-
-  // Convert all key to lowercase
-  , toLower = function(obj, skipKey) {
-    var keys = Object.keys(obj)
-    , n = keys.length;
-    while (n--) {
-      var key = keys[n]
-      , keyLower = key.toLowerCase();
-      if (key !== keyLower) {
-        obj[keyLower] = obj[key];
-        delete obj[key];
-      }
-      if (typeof obj[keyLower] === 'object') {
-        // Validation need to match exactly with the name of the input
-        if (keyLower === 'validation') {
-          continue;
-        } else if (typeof skipKey !== 'undefined'
-            && ( ( ! _.isArray(skipKey) && keyLower === skipKey )
-            || ( _.isArray(skipKey) && _.indexOf(skipKey, keyLower) > -1 )) ) {
-          continue;
+            var view = new View(options);
+            views[name] = view;
+            if (typeof context.children === 'undefined') {
+                context.children = {};
+                context.children[name] = view;
+            } else {
+                context.children[name] = view;
+            }
+            Events.trigger('viewCreated');
+            return view;
         }
-        this.toLower(obj[keyLower]);
-      }
-    }
-  }
 
-  // Parse HTML Code to normal text
-  , decodeHtml = function (obj) {
-    if (obj.fields) {
-      _.each(obj.fields, function (value, key) {
-        var _value = _.unescape(value);
-        obj.fields[key] = _value.replace(/&#39;/g, "'");
-      });
-    }
-  };
+        // Convert all key to lowercase
+        , toLower = function(obj, skipKey) {
+            var keys = Object.keys(obj),
+                n = keys.length;
+            while (n--) {
+                var key = keys[n],
+                    keyLower = key.toLowerCase();
+                if (key !== keyLower) {
+                    obj[keyLower] = obj[key];
+                    delete obj[key];
+                }
+                if (typeof obj[keyLower] === 'object') {
+                    // Validation need to match exactly with the name of the input
+                    if (keyLower === 'validation') {
+                        continue;
+                    } else if (typeof skipKey !== 'undefined' && ((!_.isArray(skipKey) && keyLower === skipKey) || (_.isArray(skipKey) && _.indexOf(skipKey, keyLower) > -1))) {
+                        continue;
+                    }
+                    this.toLower(obj[keyLower]);
+                }
+            }
+        }
+
+        // Parse HTML Code to normal text
+        , decodeHtml = function(obj) {
+            if (obj.fields) {
+                _.each(obj.fields, function(value, key) {
+                    var _value = _.unescape(value);
+                    obj.fields[key] = _value.replace(/&#39;/g, "'");
+                });
+            }
+        }
+
+        // Change Language
+        , changeLanguage = function(obj, language) {
+            _.each(obj, function (element) {
+              if (element.description && element.languages && element.languages[language]) {
+                element.description = element.languages[language];
+              }
+            });
+        };
 
 
-  return {
-    create: create,
-    remove: remove,
-    toLower: toLower,
-    decodeHtml: decodeHtml
-  };
+    return {
+        create: create,
+        remove: remove,
+        toLower: toLower,
+        decodeHtml: decodeHtml,
+        changeLanguage: changeLanguage,
+    };
 });
