d46efaaf6c5226a3dd98e88cf0efc26c079483b5
Prut Udomwattawee <apzentral@gmail.com>
2013-11-22 11:53:53 -0800

    Remove whitespace.

--- js/views/readonly/horizontal.js
+++ js/views/readonly/horizontal.js
@@ -2,77 +2,79 @@
  * Horizontal Read Mode Layout
  **/
 define([
-  'jquery',
-  'lodash',
-  'backbone',
-  'vm',
-  'utils',
-  'events',
-  'views/baseField',
-  'text!templates/readonly/horizontal.html'
-], function($, _, Backbone, Vm, Utils, Events, BaseFieldView, readLayoutTemplate){
-  var AppView = BaseFieldView.extend({
-    template: _.template(readLayoutTemplate),
-    initialize: function () {
-	  BaseFieldView.prototype.initialize.call(this);
+    'jquery',
+    'lodash',
+    'backbone',
+    'vm',
+    'utils',
+    'events',
+    'views/baseField',
+    'text!templates/readonly/horizontal.html'
+], function($, _, Backbone, Vm, Utils, Events, BaseFieldView, readLayoutTemplate) {
+    var AppView = BaseFieldView.extend({
+        template: _.template(readLayoutTemplate),
+        initialize: function() {
+            BaseFieldView.prototype.initialize.call(this);
 
-	  this._divcontrolgroup = 0;	// init div counter
+            this._divcontrolgroup = 0; // init div counter
 
-	  if (typeof this.options.formSchema === 'undefined') {
-		throw 'formSchema is not in the options parameters';
-	  }
-	  if (typeof this.options.formData === 'undefined') {
-		throw 'formData is not in the options parameters';
-	  }
-	  this.el = '#'+this.options.formSchema.name;
-	  $(this.el).addClass('form-horizontal');
-    },
-    render: function () {
-		var that = this
-		, _parentRender = BaseFieldView.prototype.render
-		, _html = '';
-	  _.each(this.options.formSchema.fields, function(value, key, list) {
+            if (typeof this.options.formSchema === 'undefined') {
+                throw 'formSchema is not in the options parameters';
+            }
+            if (typeof this.options.formData === 'undefined') {
+                throw 'formData is not in the options parameters';
+            }
+            this.el = '#' + this.options.formSchema.name;
+            $(this.el).addClass('form-horizontal');
+        },
+        render: function() {
+            var that = this,
+                _parentRender = BaseFieldView.prototype.render,
+                _html = '';
+            _.each(this.options.formSchema.fields, function(value, key, list) {
 
-		var _typeLowerCase = value.type.toLowerCase();
+                var _typeLowerCase = value.type.toLowerCase();
 
-		// Check if the data is empty, will not render
-		if ( ! Utils.isRenderReadMode(that, value)) {
-		  return '';
-		}
+                // Check if the data is empty, will not render
+                if (!Utils.isRenderReadMode(that, value)) {
+                    return '';
+                }
 
-		// VisibleOn Options
-		if (value.options.visibleon && _typeLowerCase !== 'html' &&
-			value.options.visibleon.values.indexOf(that.options.formData.fields[value.options.visibleon.name]) === -1) {
-		  return '';
-		}
+                // VisibleOn Options
+                if (value.options.visibleon && _typeLowerCase !== 'html' &&
+                    value.options.visibleon.values.indexOf(that.options.formData.fields[value.options.visibleon.name]) === -1) {
+                    return '';
+                }
 
-		// Check for Show On Mode
-		if ( ! BaseFieldView.prototype.checkShowOnMode.call(that, value, 'read', that.options.formData.status) ) {
-		  return '';
-		} else if (_typeLowerCase === 'buttondecision') {
-		  _html += '<input type="hidden" name="'+value.name+'" id="'+value.name+'_btn_condition" value="'+that.options.formData.fields[value.name]+'"/>';
-		  return '';
-		}
+                // Check for Show On Mode
+                if (!BaseFieldView.prototype.checkShowOnMode.call(that, value, 'read', that.options.formData.status)) {
+                    return '';
+                } else if (_typeLowerCase === 'buttondecision') {
+                    _html += '<input type="hidden" name="' + value.name + '" id="' + value.name + '_btn_condition" value="' + that.options.formData.fields[value.name] + '"/>';
+                    return '';
+                }
 
-		if (typeof value.description !== 'undefined' && _.indexOf(that.notRenderLabel, _typeLowerCase) === -1) {
-			_html += '<div class="control-group">';
-			this._divcontrolgroup++;
-			_html += that.renderLabel(value, false, 'control-label');
-			_html += '<div class="controls">';
-		}
-		_html += _parentRender.call(that, value, true);
-		if (typeof value.description !== 'undefined' && _.indexOf(that.notRenderLabel, _typeLowerCase) === -1) {
-			_html += '</div></div>';
-			this._divcontrolgroup--;
-		}
-	  });
-	  // not auto rendering the button
-	  //_html += BaseFieldView.prototype.renderButton.call(this, this.options.formSchema.formoptions);
+                if (typeof value.description !== 'undefined' && _.indexOf(that.notRenderLabel, _typeLowerCase) === -1) {
+                    _html += '<div class="control-group">';
+                    this._divcontrolgroup++;
+                    _html += that.renderLabel(value, false, 'control-label');
+                    _html += '<div class="controls">';
+                }
+                _html += _parentRender.call(that, value, true);
+                if (typeof value.description !== 'undefined' && _.indexOf(that.notRenderLabel, _typeLowerCase) === -1) {
+                    _html += '</div></div>';
+                    this._divcontrolgroup--;
+                }
+            });
+            // not auto rendering the button
+            //_html += BaseFieldView.prototype.renderButton.call(this, this.options.formSchema.formoptions);
 
-	  // Closed open div
-	  _html += BaseFieldView.prototype.closeOpenDiv.call(this);
-      $(this.el).html(this.template(_.extend({html : _html}, this.options.formSchema)));
-    }
-  });
-  return AppView;
+            // Closed open div
+            _html += BaseFieldView.prototype.closeOpenDiv.call(this);
+            $(this.el).html(this.template(_.extend({
+                html: _html
+            }, this.options.formSchema)));
+        }
+    });
+    return AppView;
 });
--- js/views/subform-layouts/table.js
+++ js/views/subform-layouts/table.js
@@ -2,106 +2,114 @@
  * Table Collection View Layout
  **/
 define([
-  'jquery',
-  'lodash',
-  'backbone',
-  'vm',
-  'utils',
-  'events',
-  'text!templates/subform-layouts/table.html',
-  'text!templates/notice/confirmation.html',
-  'bootstrap'
-], function($, _, Backbone, Vm, Utils, Events, tableTemplate, popoverTemplate){
-  var AppView = Backbone.View.extend({
-    template: _.template(tableTemplate),
-    popTemplate: _.template(popoverTemplate),
-    initialize: function () {
-      //console.log('=== Display List View ===');
-      //console.log(this);
-      this.collection.on('reset', this.resetCollection, this);
-    },
-    render: function () {
-      //console.log('=== Render List View ===');
-      //console.log(this.collection.toJSON());
-      var that = this
-      , _labels = []
-      , _values = new Array (this.collection.length)
-      , _models = new Array (this.collection.length);
-      _.each(this.options.formSchema.fields , function(element) {
-        _labels.push(element.description);
-        _.each(that.collection.models , function(modelObj, index) {
-          var model = modelObj.toJSON(), _fullName;
-          if (typeof _values[index] === 'undefined') {
-            _values[index] = [];
-            _models[index] = modelObj.cid;
-          }
-          switch (element.type.toLowerCase()) {
-			case 'timestamp':
-			  _labels[_labels.length-1] = 'Time';
-			  // Convert to Human Readable Time
-			  _values[index].push(Utils.getHumanTime(model[element.name]));
-			  break;
+    'jquery',
+    'lodash',
+    'backbone',
+    'vm',
+    'utils',
+    'events',
+    'text!templates/subform-layouts/table.html',
+    'text!templates/notice/confirmation.html',
+    'bootstrap'
+], function($, _, Backbone, Vm, Utils, Events, tableTemplate, popoverTemplate) {
+    var AppView = Backbone.View.extend({
+        template: _.template(tableTemplate),
+        popTemplate: _.template(popoverTemplate),
+        initialize: function() {
+            //console.log('=== Display List View ===');
+            //console.log(this);
+            this.collection.on('reset', this.resetCollection, this);
+        },
+        render: function() {
+            //console.log('=== Render List View ===');
+            //console.log(this.collection.toJSON());
+            var that = this,
+                _labels = [],
+                _values = new Array(this.collection.length),
+                _models = new Array(this.collection.length);
+            _.each(this.options.formSchema.fields, function(element) {
+                _labels.push(element.description);
+                _.each(that.collection.models, function(modelObj, index) {
+                    var model = modelObj.toJSON(),
+                        _fullName;
+                    if (typeof _values[index] === 'undefined') {
+                        _values[index] = [];
+                        _models[index] = modelObj.cid;
+                    }
+                    switch (element.type.toLowerCase()) {
+                        case 'timestamp':
+                            _labels[_labels.length - 1] = 'Time';
+                            // Convert to Human Readable Time
+                            _values[index].push(Utils.getHumanTime(model[element.name]));
+                            break;
 
-			case 'useraccount':
-			  _labels[_labels.length-1] = 'User';
-			  _values[index].push(model[element.name]);
-			  break;
+                        case 'useraccount':
+                            _labels[_labels.length - 1] = 'User';
+                            _values[index].push(model[element.name]);
+                            break;
 
-            case 'fullname':
-              _fullName = model[element.name+'_fullname_first_name'];
-              if (typeof model[element.name+'_fullname_middle_name'] !== 'undefined') {
-                _fullName += ' ' + model[element.name+'_fullname_middle_name'];
-              }
-              _fullName += ' ' + model[element.name+'_fullname_last_name'];
-              _values[index].push(_fullName);
-              break;
+                        case 'fullname':
+                            _fullName = model[element.name + '_fullname_first_name'];
+                            if (typeof model[element.name + '_fullname_middle_name'] !== 'undefined') {
+                                _fullName += ' ' + model[element.name + '_fullname_middle_name'];
+                            }
+                            _fullName += ' ' + model[element.name + '_fullname_last_name'];
+                            _values[index].push(_fullName);
+                            break;
 
-            default:
-              _values[index].push(model[element.name]);
-          }
-        });
-      });
-      $(this.el).html(this.template({ labels:_labels, values:_values, modelId: _models, heading: ( (typeof this.options.formSchema.options.readmodedescription !== 'undefined') ? this.options.formSchema.options.readmodedescription: this.options.formSchema.name) }));
-    },
-    events: {
-      'click .subform-edit-model' : 'editModel',
-      'click .subform-remove-model' : 'popoverConfirm',
-      'click .popover-action .popover-submit' : 'removeModel',
-      'click .popover-action .popover-cancel' : 'removePopover'
-    },
-    editModel: function(e) {
-      e.preventDefault();
-      var _index = $(e.currentTarget, this.el).attr('data-id');
-      $('.actions .form-view', this.$el.parent('.subform-container')).trigger('click', this.collection.get(_index));
-    },
-    popoverConfirm: function(e) {
-      e.preventDefault();
+                        default:
+                            _values[index].push(model[element.name]);
+                    }
+                });
+            });
+            $(this.el).html(this.template({
+                labels: _labels,
+                values: _values,
+                modelId: _models,
+                heading: ((typeof this.options.formSchema.options.readmodedescription !== 'undefined') ? this.options.formSchema.options.readmodedescription : this.options.formSchema.name)
+            }));
+        },
+        events: {
+            'click .subform-edit-model': 'editModel',
+            'click .subform-remove-model': 'popoverConfirm',
+            'click .popover-action .popover-submit': 'removeModel',
+            'click .popover-action .popover-cancel': 'removePopover'
+        },
+        editModel: function(e) {
+            e.preventDefault();
+            var _index = $(e.currentTarget, this.el).attr('data-id');
+            $('.actions .form-view', this.$el.parent('.subform-container')).trigger('click', this.collection.get(_index));
+        },
+        popoverConfirm: function(e) {
+            e.preventDefault();
 
-      var _opt = {
-		html : true,
-		placement: 'bottom',
-		trigger: 'manual',
-		title: 'Do you want to remove this data?',
-		content: this.popTemplate({ "id" : $(e.currentTarget, this.el).attr('data-id')})
-	  };
-	  $(e.currentTarget, this.el).popover(_opt).popover('show');
-    },
-    removeModel: function(e) {
-      e.preventDefault();
-      var _index = $(e.currentTarget, this.el).attr('data-id');
-      this.collection.remove(this.collection.get(_index));
-      if (this.collection.length > 0) {
-        this.render();
-      } else {
-        this.$el.html('');
-      }
-    },
-    removePopover: function(e) {
-      $('.subform-remove-model', $(e.currentTarget, this.el).parents('.subform-actions')).popover('destroy').next('.popover').remove();
-    },
-    resetCollection: function(e) {
-      this.$el.html('');
-    }
-  });
-  return AppView;
+            var _opt = {
+                html: true,
+                placement: 'bottom',
+                trigger: 'manual',
+                title: 'Do you want to remove this data?',
+                content: this.popTemplate({
+                    "id": $(e.currentTarget, this.el).attr('data-id')
+                })
+            };
+            $(e.currentTarget, this.el).popover(_opt).popover('show');
+        },
+        removeModel: function(e) {
+            e.preventDefault();
+            var _index = $(e.currentTarget, this.el).attr('data-id');
+            this.collection.remove(this.collection.get(_index));
+            if (this.collection.length > 0) {
+                this.render();
+            } else {
+                this.$el.html('');
+            }
+        },
+        removePopover: function(e) {
+            $('.subform-remove-model', $(e.currentTarget, this.el).parents('.subform-actions')).popover('destroy').next('.popover').remove();
+        },
+        resetCollection: function(e) {
+            this.$el.html('');
+        }
+    });
+    return AppView;
 });
