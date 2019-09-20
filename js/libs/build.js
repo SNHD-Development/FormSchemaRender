85e2d78ccc02e4ddbf6461f16953654dcb37f12d
Prut Udomwattawee <apzentral@gmail.com>
2019-09-20 14:03:13 -0700

    Update FormsRender build file for webcam

--- js/libs/build.js
+++ js/libs/build.js
@@ -124,6 +124,8 @@
     // Just a short cut so we can put our html outside the js dir
     // When you have HTML/CSS designers this aids in keeping them out of the js directory
     templates: 'templates',
-    views: 'views'
+    views: 'views',
+    cropper: "libs/images/cropper",
+    webcam: "libs/webcam/webcam"
   }
 })
